ca044bce8d0a83b68ac5f3133d1fc9191a75e13d
melaniedpm <57366712+melaniedpm@users.noreply.github.com>
2019-11-04 15:14:47 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,16 @@
 ## In this assignment, I will cache the inverse of a matrix 
-## since it is more beneficial to do so than to compute the matrix 
-## repeatedly.
+## since it is more beneficial to do so than to compute the matrix repeatedly.
 
 
 ## makeCacheMatrix is a function in which the special "matrix" object created can cache its inverse.
+## This function takes an input matrix and returns a list. 
+#1.  x is the input matrix, the default is an empty matix.
+#2.  The first step is define the function set that sets the initial value of x and m.  The <<- operator means this information is called object in an environment that is different from an enviroment different from the current function.
+#3.  The 2nd step defines the get function as makine equal to the input matrix
+#4.  The next steps assign the inverse of the matrix to m
+#5.  Lastly, the output list is created.  set data (x & m), get (input matrix), setsolve (the inverse), and getsolve (new cached m data)
+#6.  By naming the objects in the list, $ can be used to select parts.
+
 makeCacheMatrix <- function(x = matrix()) {
 inv <- NULL
         set <- function(y) { #assigns the input argument to the x object in the parent environment
@@ -20,7 +27,12 @@ inv <- NULL
 }
 
 
-## This function computes the inverse of makeCacheMatrix's matrix.
+## This function computes the inverse of makeCacheMatrix's matrix. 
+## If the original matrix has been previously tested then cached data is returned along with a note.  
+## If not this functin calculates the inverse.
+#1.  The first step is to determine if there is cached data.  If so a note and the cached data is returned
+#2.  The next step retuns the inverse if not cached
+
 cacheSolve <- function(x, ...) {
         inv <- x$getInverse() #retrieve the inverse from the object passed in as the argument
         if (!is.null(inv)) { #check to see whether the result is NULL
